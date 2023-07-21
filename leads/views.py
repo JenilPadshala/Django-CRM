940e1f479d0198fc18a1e135da450cfc65a7edca
Jenil Padshala <jenil_padshala@srmap.edu.in>
2023-07-21 15:36:12 +0530

    create categoryListView

--- leads/views.py
+++ leads/views.py
@@ -3,7 +3,7 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.views import generic
-from .models import Lead, Agent
+from .models import Lead, Agent, Category
 from .forms import LeadForm, LeadModelForm, CustomUserCreationForm, AssignAgentForm
 from django.core.mail import send_mail
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -36,7 +36,6 @@ class LeadListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         user=self.request.user
-
         # initial queryset of leads for the entire organisation
         if user.is_organiser:
             queryset = Lead.objects.filter(organisation=user.userprofile, agent__isnull=False)
@@ -199,6 +198,41 @@ class AssignAgentView(OrganiserAndLoginRequiredMixin, generic.FormView):
         lead.agent = agent
         lead.save()
         return super(AssignAgentView, self).form_valid(form)
+
+
+class CategoryListView(LoginRequiredMixin, generic.ListView):
+    template_name = "leads/category_list.html"
+    context_object_name = "category_list"
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_organiser:
+            queryset = Lead.objects.filter(
+                organisation=user.userprofile
+            )
+        else:
+            queryset = Lead.objects.filter(
+                organisation=user.agent.organisation
+            )
+        context.update({
+            "unassigned_lead_count": queryset.filter(category__isnull=True).count()
+        })
+        return context
+
+    def get_queryset(self):
+        user=self.request.user
+        # initial queryset of leads for the entire organisation
+        if user.is_organiser:
+            queryset = Category.objects.filter(
+                organisation=user.userprofile, 
+                )
+        else:
+            queryset = Category.objects.filter(
+                organisation=user.agent.organisation
+                )
+        return queryset
 
 
 # def lead_update(request, pk):
